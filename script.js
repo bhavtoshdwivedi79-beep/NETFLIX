56bf60d02baa0bb5396480f52e930438371f6665
Bhavtosh Dwivedi <bhavtoshdwivedi79@gmail.com>
2026-06-20 13:01:18 +0530

    added new file

---
+++ script.js
@@ -0,0 +1,62 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
+
+// searchbar 
+const searchInput =
+    document.getElementById("searchInput");
+
+const movieCards =
+    document.querySelectorAll(".movie-card");
+
+searchInput.addEventListener("keyup", () => {
+
+    const value =
+        searchInput.value.toLowerCase();
+
+    movieCards.forEach(card => {
+
+        const title =
+            card.dataset.title.toLowerCase();
+
+        if (title.includes(value)) {
+            card.style.display = "block";
+        }
+        else {
+            card.style.display = "none";
+        }
+
+    });
+
+});
+
+// themetoggle
+const themeToggle =
+    document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+    if (
+        document.body.classList.contains(
+            "light-mode"
+        )
+    ) {
+        themeToggle.innerHTML = "☀️";
+    }
+    else {
+        themeToggle.innerHTML = "🌙";
+    }
+
+});
