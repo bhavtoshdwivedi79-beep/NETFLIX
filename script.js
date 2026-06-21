cf98e8f0a8dbca29bb3448a04e47336fcf53afff
Bhavtosh Dwivedi <bhavtoshdwivedi79@gmail.com>
2026-06-21 17:23:49 +0530

    updating netlix

--- script.js
+++ script.js
@@ -60,3 +60,16 @@ themeToggle.addEventListener("click", () => {
     }
 
 });
+
+// hamburger menu
+const hamburger =
+    document.getElementById("hamburger");
+
+const navMenu =
+    document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+});
